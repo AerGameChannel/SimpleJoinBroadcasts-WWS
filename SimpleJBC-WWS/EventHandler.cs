4d8faeff560e9bceafea7f05c331ec8333ad6216
AerGameChannel <63594377+AerGameChannel@users.noreply.github.com>
2020-05-04 21:13:44 +0300

    Add files via upload

---
+++ SimpleJBC-WWS/EventHandler.cs
@@ -0,0 +1,18 @@
+﻿using WW_SYSTEM.EventHandlers;
+using WW_SYSTEM.Events;
+
+namespace SimpleJBC
+{
+    public class EventHandler : IEventHandlerPlayerJoin
+    {
+        public SimpleJBC plugin;
+        public EventHandler(SimpleJBC plugin)
+        {
+            this.plugin = plugin;
+        }
+        public void OnPlayerJoin(PlayerJoinEvent ev)
+        {
+            ev.Player.PersonalBroadcast(SimpleJBC.bcmsg, SimpleJBC.bctime, false);
+        }
+    }
+}
